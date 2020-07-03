9f09d9cd85618a3e70e0ac7e3e4a2db8c422dc33
Alejandro Upegui Betancur <alejandro.upegui@ias.com.co>
2020-07-03 18:26:09 -0500

    Building of Page Home and its first component Header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/styles/styles.scss";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/styles/styles.scss';
+import * as serviceWorker from './serviceWorker';
+import Home from './pages/Home';
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 serviceWorker.unregister();
