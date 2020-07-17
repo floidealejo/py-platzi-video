6c696abe0b2a72e16ebeaf73390c8e321817e7b9
Alejandro Upegui Betancur <alejandro.upegui@ias.com.co>
2020-07-17 16:52:48 -0500

    [ADD] Route for Register page

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path='/' component={Home} />
     <Route exact path='/Login' component={Login} />
+    <Route exact path='/Register' component={Register} />
   </BrowserRouter>
 );
 export default App;
