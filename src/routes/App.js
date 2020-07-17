88eeecaee5754347f25221e509858debe14b8cf1
Alejandro Upegui Betancur <alejandro.upegui@ias.com.co>
2020-07-17 15:49:14 -0500

    [ADD] Route for Login Page

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,10 +2,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path='/' component={Home} />
+    <Route exact path='/Login' component={Login} />
   </BrowserRouter>
 );
 export default App;
